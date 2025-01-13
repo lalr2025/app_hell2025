41a9fa2f3ba3df5d5f8eeb70893246c01b4a8f30
lalr2025 <luizaugusto.leiteribeiro@gmail.com>
2025-01-13 21:53:56 +0000

    Atualizando app para pegar coordenadas GPS

--- meu-app/meu-app/App.js
+++ meu-app/meu-app/App.js
@@ -1,14 +1,9 @@
+import { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import * as Location from 'expo-location';
-import { useState } from 'react';
-
-type LocationCoords = {
-  latitude: number;
-  longitude: number;
-};
 
 export default function App() {
-  const [location, setLocation] = useState<LocationCoords | null>(null);
+  const [location, setLocation] = useState(null);
 
   async function getLocation() {
     let { status } = await Location.requestForegroundPermissionsAsync();
